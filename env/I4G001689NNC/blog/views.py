5c1e306e61b05dea51fb011026abd95afff81216
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-06-27 12:30:41 +0100

    all_changes updated

---
+++ env/I4G001689NNC/blog/views.py
@@ -0,0 +1,28 @@
+from audioop import reverse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+#from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "--all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
